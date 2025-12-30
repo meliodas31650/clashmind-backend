e98a24622b29d218c3233745b7daea67e3161e32
Paul <paulbertin33000@gmail.com>
2025-12-30 02:17:02 +0100

    Remove translations debug endpoint

--- src/translations/translations.controller.ts
+++ src/translations/translations.controller.ts
@@ -1,21 +1,10 @@
 // src/translations/translations.controller.ts
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
 import { TranslationsService, Lang } from './translations.service';
 
 @Controller('api/translations')
 export class TranslationsController {
   constructor(private readonly service: TranslationsService) {}
-
-  @Get('/_debug')
-  debug() {
-    const here = __dirname;
-    const seedDir = path.join(__dirname, 'seed');
-    const exists = fs.existsSync(seedDir);
-    const files = exists ? fs.readdirSync(seedDir) : [];
-    return { here, seedDir, exists, files };
-  }
 
 
   @Get()
